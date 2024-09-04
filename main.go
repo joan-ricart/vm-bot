bafe739a6f9c09a17182fdcef2200fed20ed84a5
Joan <j.ricartt@gmail.com>
2024-09-04 08:36:39 +0200

    Added SQLite3

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/mattn/go-sqlite3"
 )
+
+const prefix string = "!vm"
 
 func main() {
 	token := os.Getenv("BOT_TOKEN")
@@ -18,13 +23,27 @@ func main() {
 		log.Fatal(err)
 	}
 
+	db, err := sql.Open("sqlite3", "./vm24.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	fmt.Println("Connected to database")
+
 	discord.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if m.Author.ID == s.State.User.ID {
 			return
 		}
 
-		if m.Content == "hello" {
-			s.ChannelMessageSend(m.ChannelID, "world!")
+		args := strings.Split(m.Content, " ")
+
+		if args[0] != prefix {
+			return
+		}
+
+		if args[1] == "hello" {
+			s.ChannelMessageSend(m.ChannelID, "Hello world")
 		}
 	})
 
